a621f54ed48ce09262de1f28240cf9d3a2081897
DimitriosMoliatsidis <dai18059@uom.edu.gr>
2021-12-02 11:00:45 +0200

    Defined USERNAME and PASSWORD of the database as static final in order to use them for each connection

--- src/SQLConnect.java
+++ src/SQLConnect.java
@@ -3,6 +3,8 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class SQLConnect {
+    static final String USERNAME="root";
+    static final String PASSWORD="root";
     public static void main(String args[]){
         String name;
         String source;
@@ -31,7 +33,7 @@ public class SQLConnect {
         try
     {
         Connection con=DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/mydb2","root","root");  // establishing connection with db
+                "jdbc:mysql://localhost:3306/mydb2",USERNAME,PASSWORD);  // establishing connection with db
         PreparedStatement pst = con.prepareStatement("insert into user (name) values (?)");
         pst.setString(1,name);
         pst.executeUpdate();
@@ -72,7 +74,7 @@ public class SQLConnect {
         try
         {
             Connection con=DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/mydb","root","root");  // establishing connection with db
+                    "jdbc:mysql://localhost:3306/mydb",USERNAME,PASSWORD);  // establishing connection with db
 
 
             Statement st = con.createStatement();
@@ -94,7 +96,7 @@ public class SQLConnect {
     public static void PrintTable(){
         try{
         Connection con=DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/mydb2","root","root");
+                "jdbc:mysql://localhost:3306/mydb2",USERNAME,PASSWORD);
 
         Statement stmt=con.createStatement();
         ResultSet rs=stmt.executeQuery("select * from user join photo on user.id=photo.user_id");
@@ -115,7 +117,7 @@ public class SQLConnect {
 
         try{
             Connection con=DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/mydb2","root","root");  // establishing connection with db
+                "jdbc:mysql://localhost:3306/mydb2",USERNAME,PASSWORD);  // establishing connection with db
             Statement stmt=con.createStatement();
             ResultSet rs=stmt.executeQuery("select id from user where name like '"+name+"' LIMIT 1");
             while (rs.next())
@@ -139,7 +141,7 @@ public class SQLConnect {
         int photoId=0;
         try {
             Connection con = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/mydb2", "root", "root");  // establishing connection with db
+                    "jdbc:mysql://localhost:3306/mydb2", USERNAME,PASSWORD);  // establishing connection with db
             Statement stmt=con.createStatement();
             ResultSet rs=stmt.executeQuery("select id from user where name like '"+name+"' LIMIT 1");
             while (rs.next())
